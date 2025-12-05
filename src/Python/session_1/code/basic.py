67b5f478d3edb81fc9a6bb6170abcd6047738899
Muhammad-Walid-Abdelzaher <mohamed.w.abdelzaher@gmail.com>
2025-12-05 20:37:27 +0200

    new session update

---
+++ src/Python/session_1/code/basic.py
@@ -0,0 +1,7 @@
+print("My name is {0}, My age is {1}".format("Muhammad", 20))
+print("My name is {0}, My age is {1}".format("Muhammad", 20))
+print("My name is {name}, My age is {age}".format(name = "Muhammad", age = 20))
+
+print("#" * 50)  # Separator
+
+print("My name is {my_name}, and I am {my_age} years old. I am engaged in learning {course_name}.".format(my_name = "Muhammad", my_age = 20, course_name = "Python"))
